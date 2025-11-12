62520ff4e971639f49a1991aa2a16cef8c32cf39
StriderCraft315 <michaelmj2540@gmail.com>
2025-11-12 19:41:52 +0530

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,40 @@
+# PASTE ALL
+echo "
+=============================================================
+
+                   NISSALOP2
+                  Maded By Nissalop2
+             Copyright (C) By Nissalop2
+             
+
+               VM V3 By HopingBoyz
+============================================================
+"
+
+git clone https://github.com/Himal0766/hvm-v3.git
+apt install unzip -y && unzip v3-hvm.zip
+cd hvm
+apt update
+apt install python3 -y
+apt install neofetch -y && neofetch
+
+apt install python3-pip  
+python3 -m pip install --upgrade pip
+echo "
+=========================================================
+
+                    HVM STARTING IN 5 Secoands
+                      Admin:
+                     Username: admin
+                     Password: admin
+
+                    Check Port (" 3000 ")
+
+                      Copyright (C) By Nissalop2
+==========================================================
+
+"
+
+pip install flask flask-socketio flask-login docker paramiko psutil python-dotenv email-validator flask_limiter
+# FINAL
+python3 hvm.py
